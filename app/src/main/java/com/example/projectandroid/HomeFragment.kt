629a2221ec0ece555c25edd541ada8df4575cc73
MTPyronaut <MeetThePyro@users.noreply.github.com>
2019-12-27 20:26:57 +0100

    nav drawer

--- app/src/main/java/com/example/projectandroid/HomeFragment.kt
+++ app/src/main/java/com/example/projectandroid/HomeFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 
@@ -27,5 +28,7 @@ class HomeFragment : Fragment() {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(HomeViewModel::class.java)
         // TODO: Use the ViewModel
+
+
     }
 }
--- app/src/main/java/com/example/projectandroid/MainActivity.kt
+++ app/src/main/java/com/example/projectandroid/MainActivity.kt
@@ -1,22 +1,70 @@
 package com.example.projectandroid
 
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
 import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.example.projectandroid.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.login_fragment.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(){
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var navController: NavController
+
+    lateinit var toolbar: Toolbar
+    lateinit var navView: NavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        val navController = this.findNavController(R.id.myNavHostFragment)
+
+        toolbar = findViewById(R.id.toolbar)
+
+        setSupportActionBar(toolbar)
+
+
+        drawerLayout = binding.drawerLayout
+        navView = binding.navView
+
+        val toggle = ActionBarDrawerToggle(
+            this, drawerLayout, toolbar, 0, 0
+        )
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+        //navView.setNavigationItemSelectedListener(this)
+
+        navController = this.findNavController(R.id.myNavHostFragment)
+
+
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
         NavigationUI.setupWithNavController(binding.navView, navController)
+
+
+
+
+
+
+
+
+
     }
+
+
+    //override fun onSupportNavigateUp(): Boolean {
+    //   val navController = this.findNavController(R.id.myNavHostFragment)
+    //   return NavigationUI.navigateUp(navController, drawerLayout)
+    //}
+
 }
